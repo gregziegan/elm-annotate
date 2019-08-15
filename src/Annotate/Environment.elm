0d0efa68c676b0b6757db7a148635859c8f45e3c
Greg Ziegan <greg.ziegan@gmail.com>
2019-08-15 13:13:35 -0500

    add some doc placeholders

--- src/Annotate/Environment.elm
+++ src/Annotate/Environment.elm
@@ -1,11 +1,30 @@
-module Annotate.Environment exposing (Environment, OperatingSystem(..))
+module Annotate.Environment exposing
+    ( Environment
+    , OperatingSystem(..)
+    )
+
+{-| Details that won't change at runtime.
 
 
+# Annotation Environment
+
+@docs Environment
+
+
+# Supported Operating Systems
+
+@docs OperatingSystem
+
+-}
+
+
+{-| -}
 type alias Environment =
     { operatingSystem : OperatingSystem
     }
 
 
+{-| -}
 type OperatingSystem
     = MacOS
     | Windows
